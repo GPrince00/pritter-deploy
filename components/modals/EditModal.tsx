27ae6627d149a46d524210c6ea0ba00751fe3feb
GPrince00 <gustavoprince0@gmail.com>
2024-06-24 14:34:26 -0300

    feat(edit): create the edit modal

---
+++ components/modals/EditModal.tsx
@@ -0,0 +1,103 @@
+import { useCallback, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useEditModal from "@/hooks/useEditModal";
+import useUser from "@/hooks/useUser";
+import Modal from "../Modal";
+import Input from "../Input";
+
+const EditModal = () => {
+  const { data: currentUser } = useCurrentUser();
+  const { mutate: mutateFetchedUser } = useUser(currentUser?.id);
+  const editModal = useEditModal();
+
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [bio, setBio] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [coverImage, setCoverImage] = useState("");
+
+  useEffect(() => {
+    setName(currentUser?.name);
+    setUsername(currentUser?.username);
+    setBio(currentUser?.bio);
+    setProfileImage(currentUser?.profileImage);
+    setCoverImage(currentUser?.coverImage);
+  }, [currentUser]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      console.log("AAA EditModal 1");
+      await axios.patch("/api/edit", {
+        name,
+        username,
+        bio,
+        profileImage,
+        coverImage,
+      });
+      console.log("AAA EditModal Done");
+
+      mutateFetchedUser();
+
+      toast.success("Updated");
+
+      editModal.onClose();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }, [
+    name,
+    username,
+    bio,
+    profileImage,
+    coverImage,
+    editModal,
+    mutateFetchedUser,
+  ]);
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Bio"
+        onChange={(e) => setBio(e.target.value)}
+        value={bio}
+        disabled={isLoading}
+      />
+    </div>
+  );
+
+  return (
+    <div>
+      <Modal
+        disabled={isLoading}
+        isOpen={editModal.isOpen}
+        title="Edit your profile"
+        actionLabel="Save"
+        onClose={editModal.onClose}
+        onSubmit={onSubmit}
+        body={bodyContent}
+      />
+    </div>
+  );
+};
+
+export default EditModal;
