8e8c03299a998411cdb57c056ea8e500d5f52bfe
GPrince00 <gustavoprince0@gmail.com>
2024-06-24 14:56:43 -0300

    bugfix(UI): fixed the error of failed hydration

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,5 @@
-import { Inter } from "next/font/google";
 import Sidebar from "@/components/layout/Sidebar";
 import FollowBar from "@/components/layout/FollowBar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -10,27 +7,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="h-screen bg-black">
-          <div className="container h-full mx-auto xl:px-30 max-w-6xl">
-            <div className="grid grid-cols-4 h-full">
-              <Sidebar />
-              <div
-                className="
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+          <Sidebar />
+          <div
+            className="
                 col-span-3
                 lg:col-span=2
                 border-x-[1px]
                 border-neutral-800
               "
-              >
-                {children}
-              </div>
-              <FollowBar />
-            </div>
+          >
+            {children}
           </div>
+          <FollowBar />
         </div>
-      </body>
-    </html>
+      </div>
+    </div>
   );
 }
