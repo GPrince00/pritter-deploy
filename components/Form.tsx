1a63c076242f732853616f3f9e94c4b3e7543bf3
GPrince00 <gustavoprince0@gmail.com>
2024-07-02 14:27:33 -0300

    feat(form): changes the url to be dymanic

--- components/Form.tsx
+++ components/Form.tsx
@@ -31,7 +31,9 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
     try {
       setIsLoading(true);
 
-      await axios.post("/api/posts", { body });
+      const url = isComment ? `/api/comments?postId=${postId}` : "/api/posts";
+
+      await axios.post(url, { body });
 
       toast.success("Tweet Created");
 
@@ -42,7 +44,7 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
     } finally {
       setIsLoading(false);
     }
-  }, [body, mutatePosts]);
+  }, [body, mutatePosts, isComment, postId]);
 
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
