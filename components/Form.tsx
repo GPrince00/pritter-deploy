060b347cfddc9f51699102501045547e9c60c9d4
GPrince00 <gustavoprince0@gmail.com>
2024-07-02 14:51:17 -0300

    feat(comment): implements the comment

--- components/Form.tsx
+++ components/Form.tsx
@@ -10,6 +10,7 @@ import useRegisterModal from "@/hooks/useRegisterModal";
 
 import Button from "./Button";
 import Avatar from "./Avatar";
+import usePost from "@/hooks/usePost";
 
 interface FormProps {
   placeholder: string;
@@ -23,6 +24,7 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
 
   const { data: currentUser } = useCurrentUser();
   const { mutate: mutatePosts } = usePosts();
+  const { mutate: mutatePost } = usePost(postId as string);
 
   const [body, setBody] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -39,12 +41,13 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
 
       setBody("");
       mutatePosts();
+      mutatePost();
     } catch (error) {
       toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
     }
-  }, [body, mutatePosts, isComment, postId]);
+  }, [body, mutatePosts, isComment, postId, mutatePost]);
 
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
